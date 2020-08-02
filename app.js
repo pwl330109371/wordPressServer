fb8d43ebffa5ed70f1acf5207d1a75133ca86308
pengwenlei <330109371@qq.com>
2020-08-02 21:59:59 +0800

    'first-commit'

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const bodyParser = require('body-parser');
 const passport = require('passport')
 const app = express()
 
-
-
 // 引入users.js
 const users = require('./routers/api/users')
 
@@ -21,11 +19,14 @@ const article = require('./routers/api/article')
 // 创建 application/x-www-form-urlencoded 编码解析
 app.use(bodyParser.urlencoded({ extended: false }))  
 // parse application/json  
-app.use(bodyParser.json())  
+app.use(bodyParser.json())
 // 引入users.js
 
 const db = require('./config/key').mongoURI
-mongoose.connect(db)
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true // 这个即是报的警告
+})
   .then(() => console.log('连接成功！'))
   .catch((err) => console.log(err))
 
@@ -41,5 +42,5 @@ app.use('/api/article', article)
 const server = app.listen(3000, ()=> {
   let host = server.address().address
   let port = server.address().port
-  console.log('应用访问的地址是:http://', host, port);
+  console.log('应用访问的地址是:http://', 'localhost', port);
 })
